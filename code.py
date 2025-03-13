74dd7467af038faf5877a9f4c60a7442b99a3702
niell-ray <85053860+niell-ray@users.noreply.github.com>
2025-03-13 20:48:59 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,38 @@
+import art
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+
+        if letter not in alphabet:
+            output_text += letter
+        else:
+
+            if encode_or_decode == "encode":
+                shifted_position = alphabet.index(letter) + shift_amount
+            elif encode_or_decode == "decode":
+                shifted_position = (alphabet.index(letter) - shift_amount)
+
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' to continue and 'no' to close: \n").lower()
+    if restart == 'no':
+        should_continue = False
+        print("Good bye.")
+
